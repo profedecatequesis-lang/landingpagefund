fbea2005621970b31a912aced8175361f7b8b306
profedecatequesis-lang <profedecatequesis@gmail.com>
2026-09-06 22:19:00 -0300

    Deploy Astro site to GitHub Pages (config + workflow)

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+// Configurado para GitHub Pages: https://profedecatequesis-lang.github.io/landingpagefund/
+export default defineConfig({
+  site: 'https://profedecatequesis-lang.github.io',
+  base: '/landingpagefund/',
+});
